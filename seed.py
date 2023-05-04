0f06c98153c25b69ff9904b9725bd754edd9d488
Halkano Malise <kudezx@gmail.com>
2023-05-05 01:34:56 +0300

    updated

--- seed.py
+++ seed.py
@@ -1,7 +1,9 @@
 from app import app
 from model import db, Production
 
-db.init_app(app)
+# db.init_app(app)
+
+print("============Seeding models Please wait===================")
 
 with app.app_context():
     Production.query.delete()
@@ -17,12 +19,17 @@ with app.app_context():
 
     production.append(p2)
 
-    p3 = Production(title="Tarzan", genre="Drama", director="Malise", budget=860000,
+    p3 = Production(title="Tarzan", genre="Adventure", director="Malise", budget=860000,
                     description="lorem the la ponteno el marada siekhero", image="https://i.stack.imgur.com/69dhp.png", ongoing=True)
 
     production.append(p3)
 
-    p4 = Production(title="Toy Story", genre="Drama", director="Mandazi", budget=596874130,
+    p4 = Production(title="Toy Story", genre="Animation", director="Mandazi", budget=596874130,
                     description="lorem the la ponteno el marada siekhero", image="https://i.stack.imgur.com/69dhp.png", ongoing=False)
 
     production.append(p4)
+
+    db.session.add_all(production)
+    db.session.commit()
+
+print("============Done seeding===================")
