d5237c61ae5fa85e2ddde3ff1483c8ce44f1e26f
Halkano Malise <kudezx@gmail.com>
2023-05-05 01:10:37 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+<!-- Run in terminal
+
+export FLASK_APP=app.py
+export FLASK_RUN_PORT=5555
+
+flask db init
+flask db revision --autogenerate -m "Created table Production"
+flask db upgrade -->
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+from model import db, Production
+
+# initialize the app
+
+app = Flask(__name__)
+
+# configuring the database
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+# rOUTES
+
+
+@app.route("/")
+def index():
+    return "Hey hallo"
+
+
+# if __name__ == '__main__':
+#     app.run(port=5000, debug=True)
